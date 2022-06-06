b7d116642ef8c503a855e44b554d773fcbb985af
santialvesz <santialvesdsc@gmail.com>
2022-06-06 10:32:08 -0700

    Task 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - program that prints exactly 
+ * main - Program that prints exactly 
  * Return: must return one
-*/
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
