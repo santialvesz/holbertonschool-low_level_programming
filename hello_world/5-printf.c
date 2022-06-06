fae209eb17caf9ce98922a174c728cc82ba86674
santialvesz <santialvesdsc@gmail.com>
2022-06-06 07:29:47 -0700

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a description of main
+ * Return: returned
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
