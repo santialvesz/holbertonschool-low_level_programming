4017c1a8014b8cbf39190fc545f597defad56c51
santialvesz <santialvesdsc@gmail.com>
2022-06-06 10:17:22 -0700

    Task 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 return (1);
 }
