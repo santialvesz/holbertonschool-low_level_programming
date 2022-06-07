792267340d491a68b11b9578464f37b05158d4ad
santialvesz <santialvesdsc@gmail.com>
2022-06-07 09:36:23 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * main - Program that prints the alphabet in lowercase and new line.
+ *   * Return: return none
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar (c);
+}
+putchar('\n');
+return (0);
+}
