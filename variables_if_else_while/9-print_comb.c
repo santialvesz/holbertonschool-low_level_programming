edc8c3b24830cf994bfb9d174801b1f592ede1bd
santialvesz <santialvesdsc@gmail.com>
2022-06-07 10:28:33 -0700

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 /**
- *  *  * main - Numbers cero to nine
- *   *  * Return: return none
- *    */
+ *  main - Numbers cero to nine
+ *  Return: return none
+ */
 int main(void)
 {
-int n;
-for (n = 48; n <= 57; n++)
-{
-putchar(n);
-if (n < 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-
-putchar('\n');
-return (0);
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
