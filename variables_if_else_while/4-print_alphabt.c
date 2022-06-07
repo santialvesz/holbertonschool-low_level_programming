1cf2b61eda6740e23ad3e59e212bac2df8595b3c
santialvesz <santialvesdsc@gmail.com>
2022-06-07 09:01:37 -0700

    Task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Removing q e for the alpha.
+ * Return: return none
+ */
 int main(void)
 {
 char c;
 for (c = 'a';  c <= 'z'; c++)
-{
-{
 if (c != 'q' && c != 'e')
-}	
+{	
 putchar(c);
 }
 putchar('\n');
 return (0);
 }
-
