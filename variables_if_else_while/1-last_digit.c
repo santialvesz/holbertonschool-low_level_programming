d1c7c16571cc36646934f46b00fda9185f054382
santialvesz <santialvesdsc@gmail.com>
2022-06-07 07:05:22 -0700

    Task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ if (n > 5)
 {
 printf("Last digit of %i is %i and is greater than 5\n", n, d);
 }
-elseif (n == 0)
+else if (n == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, d);
 }
-elseif (n < 6 && !0)
+else if (n < 6 && !0)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, d);
 }
