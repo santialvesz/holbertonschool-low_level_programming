b006abe64d4b284a24e5f3845970a00208e0a4d5
santialvesz <santialvesdsc@gmail.com>
2022-06-07 09:26:15 -0700

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * main - Numbers cero to nine
+ *   * Return: return none
+ *    */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
