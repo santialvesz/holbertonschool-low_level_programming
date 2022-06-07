34ac3ca5d18ddcf69775b12a592bf3dad7235ab9
santialvesz <santialvesdsc@gmail.com>
2022-06-07 07:21:16 -0700

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the alphabet in lowercase and new line.
+ * Return: return none
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar (c);
+}
+putchar(´\n');
+return (0);
+}
