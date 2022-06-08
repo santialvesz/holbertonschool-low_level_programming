bd198fd43474054ecb0a71f36e2d9c70b672c205
santialvesz <santialvesdsc@gmail.com>
2022-06-07 17:06:20 -0700

    Task 10

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,37 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * * main - alphabet
- * * Return: alphabet
- * */
+ *  * main - Prints all possible combinations.
+ *     * Return: Always 0.
+ */
 int main(void)
 {
-		int c;
-			int e;
-				int f;
-					int a = 0;
-						int b = 0;
-						for (c = 48 ; c <= 55 ; c++)
-						{
-							for (e = 49 + a ; e <= 56 ; e++)
-							{
-								for (f = 50 + b ; f <= 57; f++)
-								{
-									putchar (c);
-									putchar (e);
-									putchar (f);
-									if (c < 55)
-									{
-										putchar (44);
-										putchar (32);
-									}
-								}
-								b++;
-							}
-							a++;
-						}
-						putchar (10);
-						return (0);
+int digit1, digit2, digit3;
+
+for (digit1 = 0; digit1 < 8; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+{
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+putchar((digit3 % 10) + '0');						
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+						
+continue;
+																																								
+		
+putchar(',');
+										
+putchar(' ');
+													
+}
+									
+}
+}
+
+putchar('\n');
+
+return (0);
 }
