6801d9d4e6110388cb2a33902d19797fcf5fe913
santialvesz <santialvesdsc@gmail.com>
2022-06-07 17:16:37 -0700

    Task 10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-	int c;
-	int d = 49;
-	int e;
-	for (c = 48 ; c <= 56 ; c++)
+int c;
+int d = 49;
+int e;
+for (c = 48 ; c <= 56 ; c++)
 
-	{
-		for (e = d ; e <= 57 ; )
-		{
-			putchar (c);
-			putchar (e);
-			if (c < 56)
-			{
-				putchar (44);
-				putchar (32);
-			}
-			e++;
-		}
-		d++;
-	}
-	putchar (10);
-	return (0);
+{
+for (e = d ; e <= 57 ;)
+{
+putchar (c);
+putchar (e);
+if (c < 56)
+{
+putchar (44);
+putchar (32);
+}
+e++;
+}
+d++;
+}
+putchar (10);
+return (0);
 }
