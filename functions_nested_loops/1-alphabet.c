a088bcdce48fcea5564d68bd08e30cf69cb638fe
santialvesz <santialvesdsc@gmail.com>
2022-06-09 06:44:59 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase and a newline.
+ * Return: return
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,8 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
+/** _putchar - writes the character c to stdout **/
 int _putchar(char);
+/** print_alphabet - prints the alphabet**/
+void print_alphabet(void);
 
 #endif
