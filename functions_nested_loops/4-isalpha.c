a566e1f53269b37f2162c7da3900b6e5c7345c6f
santialvesz <santialvesdsc@gmail.com>
2022-06-09 09:09:31 -0700

    Task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <ctype.h>
 /**
- *  * _isalpha - check if c is lowercase
- *   * @c: parameter
- *    * Return: Return 1 or 0 in each case
- *     */
+ * * _isalpha - check if c is lowercase
+ * @c: parameter
+ * Return: Return 1 or 0 in each case
+ */
 int _isalpha(int c)
 {
 
