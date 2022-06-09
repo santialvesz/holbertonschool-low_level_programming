b7ed9023695127c93c59e69c0415254de1ab9266
santialvesz <santialvesdsc@gmail.com>
2022-06-09 16:40:39 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,12 +5,11 @@
  */
 void jack_bauer(void)
 {
-	
 int h, m;
 
 for (h = 0; h < 24; h++)
 {
-for (m = 0; < 60; m++)
+for (m = 0; m < 60; m++)
 {
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
@@ -19,6 +18,4 @@ _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
 _putchar('\n');
 }
-}
-}
-
+}			
