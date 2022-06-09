ad22627ff8bbb400374aa6e543bc41d72d962a8a
santialvesz <santialvesdsc@gmail.com>
2022-06-09 09:34:56 -0700

    Task 4

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,15 +11,18 @@ if (n > '0')
 {
 _putchar('+');
 return (1);
+_putchar('\n');
 }
 else if (n == '0')
 {
 _putchar(0);
 return (0);
+_putchar('\n')
 }
-else if (n < '0')
+else
 {
 _putchar('-');
 return ('1');
+_putchar('\n');
 }
 }
