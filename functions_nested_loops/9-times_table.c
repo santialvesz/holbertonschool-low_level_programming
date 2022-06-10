9fbcc02c4ad92a475f3b5d7e1ffdc0b0eab120aa
santialvesz <santialvesdsc@gmail.com>
2022-06-10 06:48:27 -0700

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,21 +6,35 @@
  */
 void times_table(void)
 {
+int x, y;
+for (x = 0 ; x < 10 ; x++)
+{
+for (y = 0 ; y < 10 ; y++)
 
-int a, b;
-for (a = 0; a < 10; a++)
+if (y == 0)
 {
-for (b = 0; b < 10; b++)
-{
-if (b == 0)
-{
-_putchar(a * b + '0');
+_putchar(x * y + '0');
 }
-else if (a * b >= 10)
+else if (x * y >= 10)
+{
+_putchar(' ');
+_putchar(x * y / 10 + '0');
+_putchar(x * y % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(x * y + '0');
+}
+
+if (y == 9)
+{
+_putchar('\n');
+}
+else
 {
 _putchar(',');
-_putchar(a * b / 10 + '0');
-_putchar(a * b % 10 + '0');
 }
 }
 }
