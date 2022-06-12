18699ffa5e398fe37fbec35f1dae96ec95645cec
santialvesz <santialvesdsc@gmail.com>
2022-06-12 15:06:37 -0700

    Tasl 2 3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a: The character to print
+ * @b: asdf
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
