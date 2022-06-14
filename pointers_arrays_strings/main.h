2b09fb7411898010a2fde3e87befd43c9a5477f1
santialvesz <santialvesdsc@gmail.com>
2022-06-14 09:30:38 -0700

    Task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@
 void reset_to_98(int *n);
 /** function that swap values of two integrers **/
 void swap_int(int *a, int *b);
+/** function that returns the length of a string **/
+int _strlen(char *s);
 #endif
