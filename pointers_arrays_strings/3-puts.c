730a233fbd7370f9d1d036bdaba2ce8cb0a3fefb
santialvesz <santialvesdsc@gmail.com>
2022-06-14 09:37:27 -0700

    Task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/** _puts - put sth
+ * @str: str
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+
+printf("%p\n", str);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,4 +7,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 /** function that returns the length of a string **/
 int _strlen(char *s);
+/** funtion that puts sth **/
+void _puts(char *str);
 #endif
