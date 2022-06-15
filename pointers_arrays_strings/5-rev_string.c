f8619974206fce3138c3698d98a382b5b82a4874
santialvesz <santialvesdsc@gmail.com>
2022-06-15 08:20:22 -0700

    Task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,13 @@ void rev_string(char *s)
 {
 	int i, len;
 	char e;
+
 	len = strlen(s);
 
-		for(i = 0;i < len/2; i++)
+		for (i = 0; i < len / 2; i++)
 		{
 			e = s[i];
-			s[i] = s[len - i - 1];				
-			s[len - i -1] = e;
+			s[i] = s[len - i - 1];
+			s[len - i - 1] = e;
 		}
 }
