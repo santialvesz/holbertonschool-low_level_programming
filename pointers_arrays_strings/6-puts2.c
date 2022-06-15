dca0e106bb75b8b8daff51376e063faba8001c70
santialvesz <santialvesdsc@gmail.com>
2022-06-15 08:43:37 -0700

    Task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@
 void puts2(char *str)
 {
 	int i;
-		for (i = 0; i < _strlen(str); i++)
+		for (i = 0; i < strlen(str); i++)
 		{
 			if (i % 2 == 0)
 			{
-				_putchar(i);
+				_putchar(str[i]);
 			}
 			{
 				_putchar('\n');
