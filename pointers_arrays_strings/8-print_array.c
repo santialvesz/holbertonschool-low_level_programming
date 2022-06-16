ca07bb6d34c29ab5c293a7a89f5340d900dfeeb2
santialvesz <santialvesdsc@gmail.com>
2022-06-16 08:28:35 -0700

    Task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,18 +16,15 @@ void print_array(int *a, int n)
 
 		if (!(i == n - 1))
 
-		
+		{
 			printf("%d, ", a[i]);
-		
-		
+			_putchar('\n');
+		}
 		else if (i == n - 1)
 
-			
-
-				printf("%d", a[i]);
-
-			
-
-	}
+		{
+			printf("%d", a[i]);
 			_putchar('\n');
+		}
+	}
 }
