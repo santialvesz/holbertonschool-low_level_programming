24f82746d7dbc920f231323741eb7558a410c36f
santialvesz <santialvesdsc@gmail.com>
2022-06-16 09:04:11 -0700

    Task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+/** 
+ * _strcpy - copies string
+ * @
+ * Return: return dest
+ */
+
+
 
 char *_strcpy(char *dest, char *src)
 {
-
+	
+	
 strcpy(dest, src);
-return(dest);
+
 }
