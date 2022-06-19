adb413e797c0a1fc201b123795066ab27633f64e
santialvesz <santialvesdsc@gmail.com>
2022-06-19 13:55:56 -0700

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- *  * string_toupper - writes the character c to stdout
- *   * @s: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ * string_toupper - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 char *string_toupper(char *s)
 {
-	int i;
-	for (i = 0; s[i] != 0; i++)
+int i;
+for (i = 0; s[i] != 0; i++)
+{
+	if (s[i] >= 97 && s[i] <= 122)
 	{
-			if (s[i] >= 97 && s[i] <= 122)
-					{
-								s[i] = s[i] - 32;
-									}
+		s[i] = s[i] - 32;
 	}
-	return (s);
+}
+return (s);
 }
