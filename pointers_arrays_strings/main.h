b81b15fe0aeae98cb327415709da90e4441dd28a
santialvesz <santialvesdsc@gmail.com>
2022-06-19 15:38:20 -0700

    Task 3.1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,4 +31,11 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 /** function that reverses the content of an array of integers **/
 void reverse_array(int *a, int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif
