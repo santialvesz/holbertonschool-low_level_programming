0bd86e36b68bebe8f923edf850ab3ba0f3d96f65
santialvesz <santialvesdsc@gmail.com>
2022-06-19 13:55:03 -0700

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _strcmp - writes the character c to stdout
- * @s1: The character to print
- * @s2: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _strcmp(char *s1, char *s2)
+ *  * string_toupper - writes the character c to stdout
+ *   * @s: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
+char *string_toupper(char *s)
 {
-int i = 0;
-while ((s1[i] != 0 && s2[i] != 0) && (s1[i] == s2[i]))
-{
-i++;
-}
-if (s1[i] != s2[i])
-return (s1[i] - s2[i]);
-return (0);
+	int i;
+	for (i = 0; s[i] != 0; i++)
+	{
+			if (s[i] >= 97 && s[i] <= 122)
+					{
+								s[i] = s[i] - 32;
+									}
+	}
+	return (s);
 }
