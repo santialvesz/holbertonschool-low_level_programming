30ca460d9192850aac5a2abe52a4b46947c9fe85
santialvesz <santialvesdsc@gmail.com>
2022-06-21 06:52:06 -0700

    Task 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
-/** factorial - returns the factorial of a num
+/**
+ * factorial - returns the factorial of a num
  * @n: int
  * Return: 0,1,-1 in specific case
  */
@@ -14,16 +15,16 @@ int factorial(int n)
 
 	if (n > 0)
 	{
-	m = n * factorial(n-1);
-		return(m);
+	m = n * factorial(n - 1);
+		return (m);
 	}
 	else if (n == 0)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(-1);
+		return (-1);
 	}
 
 }
