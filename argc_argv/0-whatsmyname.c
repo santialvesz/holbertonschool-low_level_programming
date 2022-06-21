83cb4b6d04d67a6a36352d206d23e9d6a24c1371
santialvesz <santialvesdsc@gmail.com>
2022-06-21 10:12:02 -0700

    Task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print its name
+ * @argc: element
+ * @argv: second elemente
+ * Return: comment
+ */
+
+int main(int argc, char *argv[])
+{
+	while(argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	exit(0);
+
+
+}
