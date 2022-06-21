1e7fced82316c3a214ed2856f7676a47a15ebeab
santialvesz <santialvesdsc@gmail.com>
2022-06-21 07:17:12 -0700

    Task 4

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - returns the square root of a num
+ * @n: int
+ * Return: data
+ */
+
+int _sqrt_recursion(int n)
+{
+	
+
+
+
+
+
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - if it is prime
+ * @n: int
+ * Return: 1 or 0 in spec case
+ */
+
+
+int is_prime_number(int n)
+{
+	if (n > 1)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+}
