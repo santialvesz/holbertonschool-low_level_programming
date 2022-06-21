2516bad89be4f351e84641c33759f1150ce2cf48
santialvesz <santialvesdsc@gmail.com>
2022-06-21 06:26:52 -0700

    Task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: char
+ * Return: i
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+		
+		if (s[i] != 0)
+		{
+			i++;
+			i = 1 + _strlen_recursion(&s[i] + 1);
+		}
+		return (i);
+}
