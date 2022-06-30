84904701c7ad43ba3b51b829300060d76fcf69cc
santialvesz <santialvesdsc@gmail.com>
2022-06-30 11:42:21 -0700

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - Func
+ * @d: Elemente of pointer
+ * Return: a
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+	free(d->name);
+	free(d->owner):
+	free(d);
+	}
+}
