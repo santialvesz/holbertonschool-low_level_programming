22a31212c35da10259946a07323332fbd14e33ee
santialvesz <santialvesdsc@gmail.com>
2022-06-30 16:55:22 -0700

    Task 0 and 1

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - funct
+ * @array: array
+ * @cmp: cmp
+ * @size: size
+ * Return: sth
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a, b;
+
+	if (size <=0)
+	{
+		return (-1);
+	}
+	if (cmp && array)
+	{
+	for (a = 0; a < size; a++)
+	{
+		b= cmp(array[a]);
+		if (x != 0)
+			return (size2);
+	}
+	}
+}
