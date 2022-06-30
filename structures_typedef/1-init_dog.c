d0a3105941d9a71c0e064c5808d2043cc8cdcab3
santialvesz <santialvesdsc@gmail.com>
2022-06-30 11:27:16 -0700

    Task 0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - init
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d)
+		{
+			d->name;
+			d->age;
+			d->owner;
+		}
+}
