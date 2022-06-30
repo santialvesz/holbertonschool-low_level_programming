6a5adc2cc4f03f3669d987faacd5a9c3672ad938
santialvesz <santialvesdsc@gmail.com>
2022-06-30 11:29:13 -0700

    Task 0

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,6 +3,7 @@
 
 /**
  * init_dog - init
+ * @d: a
  * @name: name
  * @age: age
  * @owner: owner
@@ -11,10 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d)
-		{
-			d->name = name;
-			d->age = age;
-			d->owner = owner;
-		}
+if(d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
