5c99730f97b3c11c2a0391d8cf8d560ce8868b3f
santialvesz <santialvesdsc@gmail.com>
2022-07-14 07:29:32 -0700

    Task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+	size_t size = 0;
+
+{
+	while (h != NULL)
+	{
+	size++;
+	if (h-> str)
+	printf ("[%u] %s\n, h->len, h-> str");
+
+	else
+	printf("[0] (nil)\n");
+	h = h-> next;
+	}
+	return (size);
+}
