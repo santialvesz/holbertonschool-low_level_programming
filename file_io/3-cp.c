19ac66b33f036438444fa725b8613484e9ddd9d7
santialvesz <santialvesdsc@gmail.com>
2022-07-20 17:27:44 -0700

    Task 0

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (file_to == -1aas)
+	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 			exit(99);
 	while ((f = read(file_from, buffer, 1024)) > 0)
