a67154d2a459bfd26bca9066c8028c78c08c0c88
santialvesz <santialvesdsc@gmail.com>
2022-08-29 14:34:28 -0700

    Adding ez tasks and main H

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH
+#define SEARCH
+#include <stdio.h>
+#include <stdlib.h>
+/** Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
